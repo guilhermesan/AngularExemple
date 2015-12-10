09c186bb8d96bcab55a86d7761de8c5e6ad9c4e7
Your Name <you@example.com>
2015-12-10 15:25:43 -0200

    fisrt commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the clientsApp
+ */
+angular.module('clientsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the clientsApp
+ */
+angular.module('clientsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
